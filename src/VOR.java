eeaa75e3b47b5f63fd138c02bfdd7218cb33a752
Mary Santabarbara <meds@hawaii.edu>
2017-03-19 14:37:46 -1000

    Added changes to getOBS()

--- src/VOR.java
+++ src/VOR.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 
 public class VOR {
 
 	public static void main(String[] args) {
-	
-
+		VOR vor = new VOR();
+		vor.getOBS();
 	}
 	
+	
+	/**
+	 * Asks directly what obs settings are from pilot through console.
+	 */
 	public void getOBS(){
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("What is your obs setting?");
+		int obs = scan.nextInt();
+		
+		System.out.println("Your obs is " + obs);
+		
+		scan.close();
 	}
 	
+	/**
+	 * @return
+	 */
 	public boolean isGoodSignal(){
 		return false; 
 	}

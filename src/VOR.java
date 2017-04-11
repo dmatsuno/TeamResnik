27a7f0b17d9c3da20d165603b73769b62e05fe0f
minakodoi <minako@MinakonoMacBook-Pro.local>
2017-04-10 16:08:20 -1000

    Created test cases and have isGoodSignal function

--- src/VOR.java
+++ src/VOR.java
@@ -2,43 +2,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class VOR {
+        public static void main(String[] args) {
+            VOR vor = new VOR();
 
-	public static void main(String[] args) {
-		VOR vor = new VOR();
-		
-//		int obs= vor.getOBS();
-//		int signal = vor.getSignal();
-//		
-//		String typeTF = vor.getToAndFrom(obs,signal);
-//		
-//		System.out.println("Obs: " + obs);
-//		System.out.println("Signal: " + signal);
-//		System.out.println("To or From: " + typeTF);
-//                
-//    System.out.println();
-//        
-//    int obs2= vor.getOBS();
-//    int radio = vor.randomRadio();
-//
-//    String typeTF2 = vor.getToAndFrom(obs2,radio);
-//
-//    System.out.println("Obs2: " + obs2);
-//    System.out.println("Signal2: " + radio);
-//    System.out.println("To or From (2): " + typeTF2);
+    //		int obs= vor.getOBS();
+    //		int signal = vor.getSignal();
+    //		
+    //		String typeTF = vor.getToAndFrom(obs,signal);
+    //		
+    //		System.out.println("Obs: " + obs);
+    //		System.out.println("Signal: " + signal);
+    //		System.out.println("To or From: " + typeTF);
+    //                
+    //    System.out.println();
+    //        
+    //    int obs2= vor.getOBS();
+    //    int radio = vor.randomRadio();
+    //
+    //    String typeTF2 = vor.getToAndFrom(obs2,radio);
+    //
+    //    System.out.println("Obs2: " + obs2);
+    //    System.out.println("Signal2: " + radio);
+    //    System.out.println("To or From (2): " + typeTF2);
 
-    /*
-    Test cases 360*360
-     */
-    for (int i = 0; i < 361; i++) {
-      for (int j = 0; j < 361; j++) {
-        String typeTF3 = vor.getToAndFrom(i,j);
-        System.out.println("(Test) Obs: " + i);
-        System.out.println(" (Test) Signal: " + j);
-        System.out.println(" (Test) To or From: " + typeTF3);
-        System.out.println();
-      }
-    }
-	}
+        /*
+        Test cases 360*360
+         */
+            for (int i = 0; i < 361; i++) {
+                for (int j = 0; j < 361; j++) {
+                    String typeTF3 = vor.getToAndFrom(i,j);
+                    String isGood = vor.isGoodSignal(i, j);
+                    System.out.println("(Test) Obs: " + i);
+                    System.out.println("(Test) Radio: " + j);
+                    System.out.println("(Test) To or From: " + typeTF3);
+                    System.out.println("(Test) Good or Bad: " + isGood);
+                    System.out.println();
+                }
+            }
+        }
 	
 	
 	/**
@@ -111,9 +112,114 @@ public class VOR {
 	 * If the plane is directly above the station then it will be a bad signal and will 
 	 *@return false
 	 */
-	public boolean isGoodSignal(){
-		return false; 
-	}
+	public String isGoodSignal(int obs, int signal) {
+            String result = "Error";
+            int lowest = 0;
+            int highest = 0;
+            int range1 = 0;
+            int range2 = 0;
+            if(obs < 90) { 
+                lowest = (obs - 90) + 360;		
+                highest = obs + 90;
+
+                if(signal >= lowest || signal <= highest){
+                    // FROM
+                    range1 = lowest + 80;
+                    range2 = highest - 80;
+                    if (range1 > 360) {
+                        range1 = range1 - 360;
+                        if (signal <= range1 && signal >= range2) {
+                            result = "GOOD";
+                        } else {
+                            result = "BAD";
+                        }
+                    }
+                    if (signal >= range1 || signal <= range2) {
+                        result = "GOOD";
+                    } else {
+                        result = "BAD";
+                    }
+                } else {
+                    // TO 
+                    range1 = lowest - 80;
+                    range2 = highest + 80;
+                    if (signal <= range1 && signal >= range2) {
+                        result = "GOOD";
+                    } else {
+                        result = "BAD";
+                    }
+
+                }
+            } else if(obs >= 90 && obs <= 270) {
+                lowest = obs - 90;
+                highest = obs + 90;
+
+                if(signal >= lowest && signal <= highest) {
+                    // FROM
+                    range1 = lowest + 80;
+                    range2 = highest - 80;
+                    if (signal >= range1 && signal <= range2) {
+                        result = "GOOD";
+                    } else {
+                        result = "BAD";
+                    }
+                } else {
+                    // TO
+                    range1 = lowest - 80;
+                    range2 = highest + 80;
+                    if (range2 > 360) {
+                        range2 = range2 - 360;
+                        if (signal <= range1 && signal >= range2) {
+                            result = "GOOD";
+                        } else {
+                            result = "BAD";
+                        }
+                    }
+                    if (range1 < 0) {
+                        range1 = range1 + 360;
+                        if (signal <= range1 && signal >= range2) {
+                            result = "GOOD";
+                        } else {
+                            result = "BAD";
+                        }
+                    }
+                }
+            } else {
+                lowest = obs - 90;
+                highest = (obs + 90) - 360;
+
+                if(signal >= lowest || signal <= highest ) {
+                    // FROM
+                    range1 = lowest + 80;
+                    range2 = highest - 80;
+                    // 1 = 345
+                    if (range2 < 0) {
+                        range2 = range2 + 360; // 
+                        if (signal >= range1 && signal <= range2) {
+                            result = "GOOD";
+                        } else {
+                            result = "BAD";
+                        }
+                    }
+                    if(signal >= range1 || signal <= range2 ) {
+                        result = "GOOD";
+                    } else {
+                        result = "BAD";
+                    }
+                } else {
+                    // TO
+                    range1 = lowest - 80;
+                    range2 = highest + 80;
+                    if (signal <= range1 && signal >= range2) {
+                        result = "Good";
+                    } else {
+                        result = "Bad";
+                    }
+                }
+            }
+
+            return result; 
+        }
         
         /**
         * Create a fake random radio
